a3f925513c3ec37dfa4d77b59dff564c17003479
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-09-27 23:56:41 +0300

    feat: add part of backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { InMemoryDBService } from '@nestjs-addons/in-memory-db';
+import { Purchase } from './Purchase';
+import { PurchaseDTO } from './Purchase.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private purchaseService: InMemoryDBService<Purchase>) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll(): Purchase[] {
+    return this.purchaseService.getAll();
+  }
+
+  @Get(':id')
+  get(@Param('id') id: string): Purchase {
+    return this.purchaseService.get(id);
+  }
+
+  @Post('seed')
+  seed(): Purchase[] {
+    const day = new Date(Date.now()).getDate();
+    const month = new Date(Date.now()).getMonth();
+    const year = new Date(Date.now()).getFullYear();
+    const recordFactory = (idx: number): Partial<Purchase> => ({
+      id: `${idx}`,
+      title: `Title-${idx}`,
+      price: Number(`${idx * 100 - idx * 10 + idx / 2}`),
+      date: `${day - +idx}-${month}-${year}`,
+      comment: `Awesome comment here!`,
+    });
+
+    this.purchaseService.seed(recordFactory, 5);
+
+    return this.purchaseService.getAll();
+  }
+
+  @Post()
+  create(@Body() model: PurchaseDTO): Purchase {
+    return this.purchaseService.create(model);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.purchaseService.delete(id);
   }
 }
